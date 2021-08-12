ff5b1a2dd78c7c72aaa7cb3a9776b5929495736f
Adrianna <adriannam@workmail.com>
2021-08-12 08:42:48 -0400

    added flask debug toolbar

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, redirect, render_template
+from flask_debugtoolbar import DebugToolbarExtension
 from models import db, connect_db, User
 
 app = Flask(__name__)
@@ -6,16 +7,25 @@ app.config['SQLALCHEMY_DATABASE_URI'] = "postgres:///blogly"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'ihaveasecret'
 
+# app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+toolbar = DebugToolbarExtension(app)
+
+
 connect_db(app)
 db.create_all()
 
+
 @app.route('/')
 def root():
+    """Homepage redirects to list of users."""
 
     return redirect("/users")
 
+
 @app.route('/users')
 def users_index():
+    """Show a page with info on all users"""
 
     users = User.query.order_by(User.last_name, User.first_name).all()
     return render_template('users/index.html', users=users)
@@ -23,12 +33,14 @@ def users_index():
 
 @app.route('/users/new', methods=["GET"])
 def users_new_form():
+    """Show a form to create a new user"""
 
     return render_template('users/new.html')
 
 
 @app.route("/users/new", methods=["POST"])
 def users_new():
+    """Handle form submission for creating a new user"""
 
     new_user = User(
         first_name=request.form['first_name'],
@@ -43,6 +55,7 @@ def users_new():
 
 @app.route('/users/<int:user_id>')
 def users_show(user_id):
+    """Show a page with info on a specific user"""
 
     user = User.query.get_or_404(user_id)
     return render_template('users/show.html', user=user)
@@ -50,6 +63,7 @@ def users_show(user_id):
 
 @app.route('/users/<int:user_id>/edit')
 def users_edit(user_id):
+    """Show a form to edit an existing user"""
 
     user = User.query.get_or_404(user_id)
     return render_template('users/edit.html', user=user)
@@ -57,6 +71,7 @@ def users_edit(user_id):
 
 @app.route('/users/<int:user_id>/edit', methods=["POST"])
 def users_update(user_id):
+    """Handle form submission for updating an existing user"""
 
     user = User.query.get_or_404(user_id)
     user.first_name = request.form['first_name']
@@ -71,6 +86,7 @@ def users_update(user_id):
 
 @app.route('/users/<int:user_id>/delete', methods=["POST"])
 def users_destroy(user_id):
+    """Handle form submission for deleting an existing user"""
 
     user = User.query.get_or_404(user_id)
     db.session.delete(user)
